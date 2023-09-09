6f82fbbbb0b43493c092491268804a7f7d8c473c
“Chiefopoku” <kwabenaopokujnr@gmail.com>
2023-09-08 19:55:16 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 0 to 9 and letters between a to f.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char m;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	for (m = 'a' ; m <= 'f' ; m++)
+		putchar(m);
+	putchar('\n');
+	return (0);
+}
