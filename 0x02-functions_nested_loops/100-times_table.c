4176e732f0bc99fbf84e3cfe2be3003ea36cdd9e
“Chiefopoku” <kwabenaopokujnr@gmail.com>
2023-09-12 22:42:04 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,13 +1,10 @@
 #include "main.h"
 
-
 /**
   * print_times_table - printsthe times inputed
   * starting with 0.
-  *@n: the value of the timea table printed
+  *@n: the value of the time a table printed
   */
-
-
 void print_times_table(int n)
 {
 	int num, mult, prod;
