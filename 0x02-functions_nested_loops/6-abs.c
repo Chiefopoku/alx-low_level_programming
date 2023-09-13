fc72bec7943a30ef879c48fdc30b3f59afc4342a
“Chiefopoku” <kwabenaopokujnr@gmail.com>
2023-09-12 18:30:53 -0700

    absolutes

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - function that checks for alphabetic character
+ * @a : parameter to be checked
+ * Return: -a or a;
+ */
+int _abs(int a)
+{
+	if (a < 0)
+		return (-a);
+	else if (a >= 0)
+	{
+		return (a);
+	}
+	return (0);
+}
