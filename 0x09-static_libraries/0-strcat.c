d2585a5efc4cc1d4b979d64936499c5af96452a4
“Chiefopoku” <kwabenaopokujnr@gmail.com>
2023-09-29 17:01:25 -0700

    new update

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,18 +1,29 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-
- /**
- * _strcat - Entry point
- * @dest: input value
- * @src: input value
+/**
+ * _strcat - concatenates two strings
+ * @mode: input value
+ * @worth: input value
  *
- * Return: void
+ * Return: mode
  */
-
-char *_strcat(char *dest, char *src)
+char *_strcat(char *mode, char *worth)
 {
-	return (strcat(dest, src));
+	int i;
+	int j;
+
+	i = 0;
+	while (mode[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (worth[j] != '\0')
+	{
+		mode[i] = worth[j];
+		i++;
+		j++;
+	}
+
+	mode[i] = '\0';
+	return (mode);
 }
